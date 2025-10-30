be558a4a5ca95d1179eefe49a91dcd33e3fdefed
Evillyn4125 <evillyn.damasceno@escola.pr.gov.br>
2025-10-30 09:20:31 -0300

    d

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ console.log(avanca)
 
 avanca.forEach(button =>{
     button.addEventListen('click', function(){
-
+        const atual = document.querySelector('ativo')
+        const proximoPasso = "passo-" + this.getAattribute('data-proximo')
    })
 }
 
